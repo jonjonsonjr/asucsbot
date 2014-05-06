958536ebd8398762aa2152982896a5f99e4f5c80
Jon Johnson <jonjonsonjr@gmail.com>
2014-05-06 16:17:19 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var bot = require('./lib/bot');
+
+// plugins
+require('./plugins/fortune')(bot);
+
+// reply with a greeting or say 'wat'
+bot.when(/^boten:\s*(hi|hello|hey)?/, function (res, m) {
+  if (m[1]) {
+    res.send(m[1]);
+  } else {
+    res.send('wat');
+  }
+});
+
+bot.when(/\b(ray|sucks)\b/, function (res, m) {
+  res.send('ray sucks');
+});
+
+// blaze it
+bot.every(60 * 1000, function (res) {
+  var now = new Date();
+  var hour = ((now.getHours() + 11) % 12) + 1;
+  var min = now.getMinutes();
+
+  if (hour === 4 && min === 20) {
+    res.send('420 blaze it');
+  }
+});
+
+bot.connect('boten', 'password', '#asucs');
---
+++ plugins/fortune.js
@@ -0,0 +1,28 @@
+module.exports = function (bot) {
+  var fortunes = [
+    "Your future is certain",
+    "Reply hazy, try again",
+    "Bad luck",
+    "Horrible luck",
+    "Terrible luck",
+    "Good luck",
+    "Things will be looking up soon",
+    "Godly luck",
+    "You will meet a dark handsome stranger",
+    "You will slave the world",
+    "Today's weather is set for a balmy sixty degrees",
+    "No prequizzes for systems today",
+    "No food or drink in the labs",
+    "Ray sucks",
+    "No chemistry majors in the lab today",
+    "Emacs is better than vim",
+    "Vim is better than emacs"
+  ];
+
+  bot.when(/^!fortune$/, function (res, m) {
+    var i = Math.floor(Math.random() * fortunes.length);
+    var f = fortunes[i];
+
+    res.send(f);
+  });
+};
